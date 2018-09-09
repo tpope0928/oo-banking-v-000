e9f24197d40dedae8aaed05762436fa250c55a13
Thomas Pope <tpope0928@gmail.com>
2018-09-09 22:03:21 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,6 +14,12 @@ class Transfer
   end
   
   def execute_transaction
-    
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      reject_transfer
+    end
   end
 end
