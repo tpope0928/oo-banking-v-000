618976c0bd6d2b04f8ce18fdca00f6aacf29672e
Thomas Pope <tpope0928@gmail.com>
2018-09-09 22:04:45 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,5 +24,17 @@ class Transfer
   end
   
   def reverse_transfer
-    if valid? 
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      reject_transfer
+    end
+  end
+
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
 end
